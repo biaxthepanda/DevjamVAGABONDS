2ee6af33422bcf51b17813c46894b6047c068da0
biaxthepanda <biaxgamestudios@gmail.com>
2022-05-13 21:26:33 +0300

    Player Controller

---
+++ Ankara/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D _rb;
+    private Vector3 _input;
+    public float speed;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        _rb.MovePosition(transform.position + _input * speed);
+    }
+    
+    private void GetInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0);
+    }
+
+}
