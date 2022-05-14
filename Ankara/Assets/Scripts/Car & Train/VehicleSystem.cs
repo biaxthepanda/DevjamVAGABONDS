5eff3fed8d9418068a16c6e536cb40301ce12584
biaxthepanda <biaxgamestudios@gmail.com>
2022-05-15 01:01:56 +0300

    Vehicle System Fix

--- Ankara/Assets/Scripts/Car & Train/VehicleSystem.cs
+++ Ankara/Assets/Scripts/Car & Train/VehicleSystem.cs
@@ -20,20 +20,42 @@ public class VehicleSystem : MonoBehaviour
     {
        for(int i = 0; i < spawnPoses.Length; i++)
         {
-           
-            Vector2 middleVector = (new Vector2(1, 1) - new Vector2(0, 0));
-            if(spawnPoses[i].transform.position.y > middleVector.y && spawnPoses[i].transform.position.x < middleVector.x)
+            Debug.Log("1");
+            Vector2 middleVector = (new Vector2(0.66f, 0.33f)- new Vector2(0, 0));
+
+            Debug.Log(Vector2.SignedAngle(middleVector, new Vector2(spawnPoses[i].position.x, spawnPoses[i].position.y) - new Vector2(0, 0)));
+            if (Vector2.SignedAngle(middleVector, new Vector2(spawnPoses[i].position.x, spawnPoses[i].position.y) - new Vector2(0, 0)) > 0)
             {
+                Debug.Log("qweqwe");
+                GameObject instantiated = Instantiate(vehicleLeft, spawnPoses[i]);
+                instantiated.GetComponent<Vehicle>().leftToRight = true;
+            }
+            else if (Vector2.SignedAngle(middleVector, new Vector2(spawnPoses[i].position.x, spawnPoses[i].position.y) - new Vector2(0, 0)) <= 0)
+            {
+                Debug.Log("asdasdssa");
+
+                GameObject instantiated = Instantiate(vehicleRight, spawnPoses[i]);
+                instantiated.GetComponent<Vehicle>().leftToRight = false;
+            }
+
+            /*
+            if (spawnPoses[i].transform.position.y > middleVector.y && spawnPoses[i].transform.position.x < middleVector.x)
+            {
+                Debug.Log("2");
+
                 GameObject instantiated = Instantiate(vehicleLeft, spawnPoses[i]);
                 instantiated.GetComponent<Vehicle>().leftToRight = true;
             }
             else if (spawnPoses[i].transform.position.y < middleVector.y && spawnPoses[i].transform.position.x > middleVector.x)
             {
+                Debug.Log("3");
+
                 GameObject instantiated = Instantiate(vehicleRight, spawnPoses[i]);
                 instantiated.GetComponent<Vehicle>().leftToRight = false;
             }
-
+            */
             
+
 
         }
     }
